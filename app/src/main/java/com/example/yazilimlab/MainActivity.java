06934d26b0ba64d145b4ba44f0e98f04a9a5471a
Serhatkacmaz <56757412+Serhatkacmaz@users.noreply.github.com>
2021-10-06 13:42:55 +0300

    ögrenci ve admin için ayrı fragment dosyaları açıldı ve login sayfasında yönlendirme için bottom navigation bar eklendi

--- app/src/main/java/com/example/yazilimlab/MainActivity.java
+++ app/src/main/java/com/example/yazilimlab/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.yazilimlab;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.yazilimlab.LoginFragment.AdminLoginFragment;
+import com.example.yazilimlab.LoginFragment.StudentLoginFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private AdminLoginFragment adminLoginFragment;
+    private StudentLoginFragment studentLoginFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.login_bottomNavigationView);
+        adminLoginFragment = new AdminLoginFragment();
+        studentLoginFragment = new StudentLoginFragment();
+        setFragment(studentLoginFragment);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.item_bottomBar_loginMenu_studentLogin:
+                        setFragment(studentLoginFragment);
+                        return true;
+                    case R.id.item_bottomBar_loginMenu_adminLogin:
+                        setFragment(adminLoginFragment);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
     }
+
+    //https://www.youtube.com/watch?v=VUHMdkKUXxk&list=PL20Zn-5nPIPHvLPq5xJTTImOd0qeNd9rW&index=92
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.login_frameLayout, fragment);
+        transaction.commit();
+    }
+    //https://www.youtube.com/watch?v=VUHMdkKUXxk&list=PL20Zn-5nPIPHvLPq5xJTTImOd0qeNd9rW&index=92
+
 }
