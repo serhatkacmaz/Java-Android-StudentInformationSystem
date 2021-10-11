58e51cff2845507e5186ead939c6dcf939d6d4ec
Serhatkacmaz <56757412+Serhatkacmaz@users.noreply.github.com>
2021-10-11 18:36:11 +0300

    basvuruya yönlendiren sayfa tasarımı yapıldı.

--- app/src/main/java/com/example/yazilimlab/StudentHomeFragment/MakeApplicationFragment.java
+++ app/src/main/java/com/example/yazilimlab/StudentHomeFragment/MakeApplicationFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -14,12 +15,16 @@ import android.widget.ImageView;
 
 import com.example.yazilimlab.Catogery.IntibakActivity;
 import com.example.yazilimlab.Catogery.YazOkuluActivity;
+import com.example.yazilimlab.MainActivity;
 import com.example.yazilimlab.R;
+import com.example.yazilimlab.RegisterActivity;
+
+import java.sql.SQLOutput;
 
 public class MakeApplicationFragment extends Fragment {
 
 
-    private ImageView admin_home_activity_YazOkulu,admin_home_activity_intibakBasvuru;
+    private CardView cardView_makeApp_YazOkul,cardView_makeApp_YatayGecis,cardView_makeApp_DGS,cardView_makeApp_CAP,cardView_makeApp_Intibak;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,18 +37,43 @@ public class MakeApplicationFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //initializings
-        admin_home_activity_YazOkulu = (ImageView) view.findViewById(R.id.admin_home_activity_YazOkulu);
-        admin_home_activity_intibakBasvuru = (ImageView) view.findViewById(R.id.admin_home_activity_intibakBasvuru);
+        cardView_makeApp_YazOkul=(CardView) view.findViewById(R.id.cardView_makeApp_YazOkul);
+        cardView_makeApp_YatayGecis=(CardView) view.findViewById(R.id.cardView_makeApp_YatayGecis);
+        cardView_makeApp_DGS=(CardView) view.findViewById(R.id.cardView_makeApp_DGS);
+        cardView_makeApp_CAP=(CardView) view.findViewById(R.id.cardView_makeApp_CAP);
+        cardView_makeApp_Intibak=(CardView) view.findViewById(R.id.cardView_makeApp_Intibak);
 
-        //click eventler
-        admin_home_activity_YazOkulu.setOnClickListener(new View.OnClickListener() {
+
+        // click events
+        cardView_makeApp_YazOkul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getActivity(), YazOkuluActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
             }
         });
-        admin_home_activity_intibakBasvuru.setOnClickListener(new View.OnClickListener() {
+
+        cardView_makeApp_YatayGecis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), YazOkuluActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
+        cardView_makeApp_DGS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), YazOkuluActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
+        cardView_makeApp_CAP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), YazOkuluActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
+        cardView_makeApp_Intibak.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getActivity(), IntibakActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
