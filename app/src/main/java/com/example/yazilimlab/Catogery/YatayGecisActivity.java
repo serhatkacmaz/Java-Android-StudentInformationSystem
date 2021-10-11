df0fb2b37809056dabb4fa702c564167885ca8a0
Serhatkacmaz <56757412+Serhatkacmaz@users.noreply.github.com>
2021-10-11 19:03:28 +0300

    eksik basvuru sayfaları eklendi ve yönlendirme verildi.

---
+++ app/src/main/java/com/example/yazilimlab/Catogery/YatayGecisActivity.java
@@ -0,0 +1,16 @@
+package com.example.yazilimlab.Catogery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.yazilimlab.R;
+
+public class YatayGecisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yatay_gecis);
+    }
+}
--- app/src/main/java/com/example/yazilimlab/StudentHomeFragment/MakeApplicationFragment.java
+++ app/src/main/java/com/example/yazilimlab/StudentHomeFragment/MakeApplicationFragment.java
@@ -13,7 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.example.yazilimlab.Catogery.CapActivity;
+import com.example.yazilimlab.Catogery.DgsActivity;
 import com.example.yazilimlab.Catogery.IntibakActivity;
+import com.example.yazilimlab.Catogery.YatayGecisActivity;
 import com.example.yazilimlab.Catogery.YazOkuluActivity;
 import com.example.yazilimlab.MainActivity;
 import com.example.yazilimlab.R;
@@ -24,7 +27,7 @@ import java.sql.SQLOutput;
 public class MakeApplicationFragment extends Fragment {
 
 
-    private CardView cardView_makeApp_YazOkul,cardView_makeApp_YatayGecis,cardView_makeApp_DGS,cardView_makeApp_CAP,cardView_makeApp_Intibak;
+    private CardView cardView_makeApp_YazOkul, cardView_makeApp_YatayGecis, cardView_makeApp_DGS, cardView_makeApp_CAP, cardView_makeApp_Intibak;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,11 +40,11 @@ public class MakeApplicationFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        cardView_makeApp_YazOkul=(CardView) view.findViewById(R.id.cardView_makeApp_YazOkul);
-        cardView_makeApp_YatayGecis=(CardView) view.findViewById(R.id.cardView_makeApp_YatayGecis);
-        cardView_makeApp_DGS=(CardView) view.findViewById(R.id.cardView_makeApp_DGS);
-        cardView_makeApp_CAP=(CardView) view.findViewById(R.id.cardView_makeApp_CAP);
-        cardView_makeApp_Intibak=(CardView) view.findViewById(R.id.cardView_makeApp_Intibak);
+        cardView_makeApp_YazOkul = (CardView) view.findViewById(R.id.cardView_makeApp_YazOkul);
+        cardView_makeApp_YatayGecis = (CardView) view.findViewById(R.id.cardView_makeApp_YatayGecis);
+        cardView_makeApp_DGS = (CardView) view.findViewById(R.id.cardView_makeApp_DGS);
+        cardView_makeApp_CAP = (CardView) view.findViewById(R.id.cardView_makeApp_CAP);
+        cardView_makeApp_Intibak = (CardView) view.findViewById(R.id.cardView_makeApp_Intibak);
 
 
         // click events
@@ -55,21 +58,21 @@ public class MakeApplicationFragment extends Fragment {
         cardView_makeApp_YatayGecis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getActivity(), YazOkuluActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                startActivity(new Intent(getActivity(), YatayGecisActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
             }
         });
 
         cardView_makeApp_DGS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getActivity(), YazOkuluActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                startActivity(new Intent(getActivity(), DgsActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
             }
         });
 
         cardView_makeApp_CAP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getActivity(), YazOkuluActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                startActivity(new Intent(getActivity(), CapActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
             }
         });
 
