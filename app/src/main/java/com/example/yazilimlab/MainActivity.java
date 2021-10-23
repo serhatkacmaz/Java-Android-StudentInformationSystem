fd7bd34549c93eb760a35bd4744b23e9d0153bcc
Serhatkacmaz <56757412+Serhatkacmaz@users.noreply.github.com>
2021-10-23 22:24:10 +0300

    kod düzenleme ve koyu mod entegre edidli.

--- app/src/main/java/com/example/yazilimlab/MainActivity.java
+++ app/src/main/java/com/example/yazilimlab/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.yazilimlab;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); // koyu mod
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.login_bottomNavigationView);
         adminLoginFragment = new AdminLoginFragment();
