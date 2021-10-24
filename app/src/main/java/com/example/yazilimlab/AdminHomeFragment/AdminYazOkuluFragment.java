4d697e0602738605c33d0e2f7f0883fc5ca42629
Serhatkacmaz <56757412+Serhatkacmaz@users.noreply.github.com>
2021-10-25 01:41:53 +0300

    admin panel, yaz okul kategorik listeleme tamamlandı.

--- app/src/main/java/com/example/yazilimlab/AdminHomeFragment/AdminYazOkuluFragment.java
+++ app/src/main/java/com/example/yazilimlab/AdminHomeFragment/AdminYazOkuluFragment.java
@@ -18,9 +18,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import com.example.yazilimlab.Model.AdminAcceptList;
 import com.example.yazilimlab.Model.AdminAppItemAdapter;
 import com.example.yazilimlab.Model.AdminAppItemInfo;
 import com.example.yazilimlab.Model.AdminIncomingList;
+import com.example.yazilimlab.Model.AdminRejectedList;
 import com.example.yazilimlab.Model.CustomDialog;
 import com.example.yazilimlab.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -39,16 +41,29 @@ public class AdminYazOkuluFragment extends Fragment {
 
     // RecyclerViewIncoming
     RecyclerView admin_YazOkuluRecyclerViewIncoming;
-    ArrayList<AdminAppItemInfo> adminAppItemInfoIncomingArrayList;
     AdminAppItemAdapter adminAppItemIncomingAdapter;
 
-    // CardView Extand
-    private CardView admin_YazOkuluCardViewIncoming;
-    private LinearLayout admin_YazOkuluLinearLayoutIncoming;
+    // RecyclerViewAccept
+    RecyclerView admin_YazOkuluRecyclerViewAccept;
+    AdminAppItemAdapter adminAppItemAcceptAdapter;
 
+    // RecyclerViewRejected
+    RecyclerView admin_YazOkuluRecyclerViewRejected;
+    AdminAppItemAdapter adminAppItemRejectedAdapter;
+
+
+    // CardView Extand
+    private CardView admin_YazOkuluCardViewIncoming, admin_YazOkuluCardViewAccept, admin_YazOkuluCardViewRejected;
+    private LinearLayout admin_YazOkuluLinearLayoutIncoming, admin_YazOkuluLinearLayoutAccept, admin_YazOkuluLinearLayoutRejected;
 
     // Admin IncomingList
     private AdminIncomingList adminIncomingList;
+
+    // Admin AcceptList
+    private AdminAcceptList adminAcceptList;
+
+    // Admin RejectedList
+    private AdminRejectedList adminRejectedList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -64,17 +79,34 @@ public class AdminYazOkuluFragment extends Fragment {
 
         // RecyclerView
         admin_YazOkuluRecyclerViewIncoming = (RecyclerView) view.findViewById(R.id.admin_YazOkuluRecyclerViewIncoming);
+        admin_YazOkuluRecyclerViewAccept = (RecyclerView) view.findViewById(R.id.admin_YazOkuluRecyclerViewAccept);
+        admin_YazOkuluRecyclerViewRejected = (RecyclerView) view.findViewById(R.id.admin_YazOkuluRecyclerViewRejected);
 
         // CardView
         admin_YazOkuluCardViewIncoming = (CardView) view.findViewById(R.id.admin_YazOkuluCardViewIncoming);
+        admin_YazOkuluCardViewAccept = (CardView) view.findViewById(R.id.admin_YazOkuluCardViewAccept);
+        admin_YazOkuluCardViewRejected = (CardView) view.findViewById(R.id.admin_YazOkuluCardViewRejected);
 
         // LinearLayout
         admin_YazOkuluLinearLayoutIncoming = (LinearLayout) view.findViewById(R.id.admin_YazOkuluLinearLayoutIncoming);
+        admin_YazOkuluLinearLayoutAccept = (LinearLayout) view.findViewById(R.id.admin_YazOkuluLinearLayoutAccept);
+        admin_YazOkuluLinearLayoutRejected = (LinearLayout) view.findViewById(R.id.admin_YazOkuluLinearLayoutRejected);
 
         // admin IncomingList
         adminIncomingList = new AdminIncomingList(getActivity(), admin_YazOkuluRecyclerViewIncoming, admin_YazOkuluCardViewIncoming, admin_YazOkuluLinearLayoutIncoming, "Yaz Okulu", "1");
         admin_YazOkuluCardViewIncoming = adminIncomingList.admin_CardViewIncoming();
         adminAppItemIncomingAdapter = adminIncomingList.adminAppItemAdapter();
+
+
+        // admin AcceptList
+        adminAcceptList = new AdminAcceptList(getActivity(), admin_YazOkuluRecyclerViewAccept, admin_YazOkuluCardViewAccept, admin_YazOkuluLinearLayoutAccept, "Yaz Okulu", "2");
+        admin_YazOkuluCardViewAccept = adminAcceptList.admin_CardViewIncoming();
+        adminAppItemAcceptAdapter = adminAcceptList.adminAppItemAdapter();
+
+        // admin RejectedList
+        adminRejectedList = new AdminRejectedList(getActivity(), admin_YazOkuluRecyclerViewRejected, admin_YazOkuluCardViewRejected, admin_YazOkuluLinearLayoutRejected, "Yaz Okulu", "3");
+        admin_YazOkuluCardViewRejected = adminRejectedList.admin_CardViewIncoming();
+        adminAppItemRejectedAdapter = adminRejectedList.adminAppItemAdapter();
 
 
         // gelen Basvurular CardView extend event
@@ -87,6 +119,34 @@ public class AdminYazOkuluFragment extends Fragment {
                 int v = (admin_YazOkuluRecyclerViewIncoming.getVisibility() == View.GONE) ? View.VISIBLE : View.GONE;
                 TransitionManager.beginDelayedTransition(admin_YazOkuluLinearLayoutIncoming, new AutoTransition());
                 admin_YazOkuluRecyclerViewIncoming.setVisibility(v);
+                // https://www.youtube.com/watch?v=qIJ_U51s4ls&list=PLY0RqCbhFOzJZCQQ07rTTIt2YCGIxsR5-&index=18&t=421s
+            }
+        });
+
+        // onaylanan basvurular CardView extend event
+        admin_YazOkuluCardViewAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            public void onClick(View view) {
+                refresh();
+                // https://www.youtube.com/watch?v=qIJ_U51s4ls&list=PLY0RqCbhFOzJZCQQ07rTTIt2YCGIxsR5-&index=18&t=421s
+                int v = (admin_YazOkuluRecyclerViewAccept.getVisibility() == View.GONE) ? View.VISIBLE : View.GONE;
+                TransitionManager.beginDelayedTransition(admin_YazOkuluLinearLayoutAccept, new AutoTransition());
+                admin_YazOkuluRecyclerViewAccept.setVisibility(v);
+                // https://www.youtube.com/watch?v=qIJ_U51s4ls&list=PLY0RqCbhFOzJZCQQ07rTTIt2YCGIxsR5-&index=18&t=421s
+            }
+        });
+
+        // red edilen basvurular CardView extend event
+        admin_YazOkuluCardViewRejected.setOnClickListener(new View.OnClickListener() {
+            @Override
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            public void onClick(View view) {
+                refresh();
+                // https://www.youtube.com/watch?v=qIJ_U51s4ls&list=PLY0RqCbhFOzJZCQQ07rTTIt2YCGIxsR5-&index=18&t=421s
+                int v = (admin_YazOkuluRecyclerViewRejected.getVisibility() == View.GONE) ? View.VISIBLE : View.GONE;
+                TransitionManager.beginDelayedTransition(admin_YazOkuluLinearLayoutRejected, new AutoTransition());
+                admin_YazOkuluRecyclerViewRejected.setVisibility(v);
                 // https://www.youtube.com/watch?v=qIJ_U51s4ls&list=PLY0RqCbhFOzJZCQQ07rTTIt2YCGIxsR5-&index=18&t=421s
             }
         });
@@ -113,6 +173,58 @@ public class AdminYazOkuluFragment extends Fragment {
                 System.out.println("indirme");
             }
         });
+
+        // onaylanan basvurlar item click
+        adminAppItemAcceptAdapter.setOnItemClickListener(new AdminAppItemAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdminAppItemInfo adminAppItemInfo, int position) {
+                System.out.println(" onaylanan item");
+            }
+
+            @Override
+            public void onAcceptClick(AdminAppItemInfo adminAppItemInfo, int position) {
+                System.out.println(" onaylanan kabul");
+            }
+
+            @Override
+            public void onRejectClick(AdminAppItemInfo adminAppItemInfo, int position) {
+                System.out.println(" onaylanan red");
+            }
+
+            @Override
+            public void onDownloadClick(AdminAppItemInfo adminAppItemInfo, int position) {
+                System.out.println(" onaylanan indirme");
+            }
+        });
+
+        // red edilen basvurlar item click
+        adminAppItemRejectedAdapter.setOnItemClickListener(new AdminAppItemAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdminAppItemInfo adminAppItemInfo, int position) {
+                System.out.println(" red edilen item");
+            }
+
+            @Override
+            public void onAcceptClick(AdminAppItemInfo adminAppItemInfo, int position) {
+                System.out.println(" red edilen kabul");
+            }
+
+            @Override
+            public void onRejectClick(AdminAppItemInfo adminAppItemInfo, int position) {
+                System.out.println(" red edilen red");
+            }
+
+            @Override
+            public void onDownloadClick(AdminAppItemInfo adminAppItemInfo, int position) {
+                System.out.println(" red edilen indirme");
+            }
+        });
+    }
+
+    private void refresh() {
+        adminIncomingList.refreshIncomingCardView();
+        adminAcceptList.refreshIncomingCardView();
+        adminRejectedList.refreshIncomingCardView();
     }
 
 }
